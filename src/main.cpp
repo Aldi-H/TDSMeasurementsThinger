cb3625b3a034fa0b8b4952f3f8f18608d91f8a9a
Aldi-H <aldihimawan88@gmail.com>
2023-07-25 12:22:16 +0700

    feat(readTDS): add readTDS function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,66 @@
+//! Library Initialization Goes Here!
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//* TDS Library
+#include "GravityTDS.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//* Temperature Library
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//* RTC Library
+#include <SPI.h>
+#include "RTClib.h"
+
+//! Variable and PIN Initialization  Goes Here!
+//* TDS PIN
+#define TDS_PIN 34
+
+//! Variable Instance Goes Here!
+//* TDS Library Insatance
+GravityTDS gravityTds;
+
+//* Temperature Library Instance
+OneWire oneWire(33);
+DallasTemperature DS18B20_Sensor(&oneWire);
+
+//* Global Variable TDS and Temperature
+//* Initial Value for TDS and Temperature
+float tdsValue = 0;
+float temperatureValue = 0;
+
+//! Function Declaration Goes Here!
+//* FUnction declaration for readTDS
+void readTDS();
+
+//! Main Program Goes Here!
+void setup()
+{
+  Serial.begin(115200);
+
+  //* Gravity TDS Setup
+  gravityTds.setPin(TDS_PIN);
+  gravityTds.setAref(3.3);
+  gravityTds.setAdcRange(4096);
+  gravityTds.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  readTDS();
 }
+//! MAIN PROGRAM END HERE!
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+//! Function Definition Goes Here!
+//* readTDS() function definition
+void readTDS()
+{
+  DS18B20_Sensor.requestTemperatures();
+  temperatureValue = DS18B20_Sensor.getTempCByIndex(0);
+  gravityTds.setTemperature(temperatureValue);
+  gravityTds.update();
+  tdsValue = gravityTds.getTdsValue();
+
+  Serial.printf("Temperature: %.2fC ", temperatureValue);
+  Serial.printf("| TDS Value: %.2fppm \n", tdsValue);
 }
